4ce75d7842e8941633cc152dd1d62ac4a1eb7e98
YAN Yuchen <cirahnitex@gmail.com>
2018-11-26 14:36:30 +0800

    added some basic arithmetic operations

--- dy_operations.hpp
+++ dy_operations.hpp
@@ -1824,15 +1824,27 @@ namespace tg {
  */
     inline tensor weight_norm(const tensor &w, const tensor &g) { return dynet::weight_norm(w, g); }
 
+    inline tensor operator+(const tensor& x, const tensor& y) {return dynet::operator+(x, y);}
+    inline tensor operator+(const tensor& x, float y) {return dynet::operator+(x, y);}
+    inline tensor operator+(const float x, const tensor& y) {return dynet::operator+(x, y);}
     inline tensor operator+(const tensor& x, const Parameter& y) {return x + dy::tensor(y);}
     inline tensor operator+(const Parameter& x, const tensor& y) {return dy::tensor(x) + y;}
 
+    inline tensor operator-(const tensor& x, const tensor& y) {return dynet::operator-(x, y);}
+    inline tensor operator-(const tensor& x, float y) {return dynet::operator-(x, y);}
+    inline tensor operator-(float x, const tensor& y) {return dynet::operator-(x, y);}
+    inline tensor operator-(const tensor& x) {return dynet::operator-(x);}
     inline tensor operator-(const tensor& x, const Parameter& y) {return x - dy::tensor(y);}
     inline tensor operator-(const Parameter& x, const tensor& y) {return dy::tensor(x) - y;}
 
+    inline tensor operator*(const tensor& x, const tensor& y) {return dynet::operator*(x, y);}
+    inline tensor operator*(const tensor& x, float y) {return dynet::operator*(x, y);}
+    inline tensor operator*(float x, const tensor& y) {return dynet::operator*(x, y);}
     inline tensor operator*(const tensor& x, const Parameter& y) {return x * dy::tensor(y);}
     inline tensor operator*(const Parameter& x, const tensor& y) {return dy::tensor(x) * y;}
 
+    inline tensor operator/(const tensor& x, const tensor& y) {return dynet::operator/(x, y);}
+    inline tensor operator/(const tensor& x, float y) {return dynet::operator/(x, y);}
     inline tensor operator/(const tensor& x, const Parameter& y) {return x / dy::tensor(y);}
     inline tensor operator/(const Parameter& x, const tensor& y) {return dy::tensor(x) / y;}
 
--- main.cpp
+++ main.cpp
@@ -1,36 +1,19 @@
 #include <iostream>
 #include "dy.hpp"
-#include "dy_training_framework.hpp"
-
-#include <functional>
-#include <word2vec.hpp>
-#include <ECMAScript_string_utils.hpp>
-#include <sstream>
 
 using namespace tg;
 using namespace std;
 
-template<class T>
-void print_helper(const T& x, std::ostream& os=std::cout) {
-  for(const auto& t:x) {
-    os << t << " ";
-  }
-  os <<endl;
-}
-
-dy::linear_layer fc1;
-dy::linear_layer fc2;
 
 int main() {
   dy::initialize();
 
-  dy::linear_layer fc1(4);
-  dy::linear_layer fc2(1);
-  dy::tensor x({1,0});
-  x = fc1.forward(x);
-  x = dy::tanh(x);
-  x = fc2.forward(x);
-  cout << (x.as_scalar() > 0)  <<endl;
-
+  dy::tensor W({1, 2, 3, 4, 5, 6}, {3, 2});
+  dy::tensor x({-1, 0});
+  dy::tensor y({1,1});
+  auto c = x+y;
+  for(const auto val:c.as_vector()) {
+    cout << val <<endl;
+  }
   return 0;
 }
