fa47f09ffd724ca6f0247bd2c17ea11b76cffda4
YAN Yuchen <cirahnitex@gmail.com>
2019-01-22 19:08:32 +0800

    add binary readout model

---
+++ dy_binary_readout_model.hpp
@@ -0,0 +1,34 @@
+//
+// Created by YAN Yuchen on 1/22/2019.
+//
+
+#ifndef DYNET_WRAPPER_DY_BINARY_READOUT_MODEL_HPP
+#define DYNET_WRAPPER_DY_BINARY_READOUT_MODEL_HPP
+
+#include "dy_common.hpp"
+#include "dy_serialization_helper.hpp"
+#include "dy_linear_layer.hpp"
+
+namespace tg {
+  namespace dy {
+    class binary_readout_model {
+      linear_layer fc;
+    public:
+      EASY_SERIALZABLE(fc)
+      binary_readout_model():fc(1){};
+      binary_readout_model(const binary_readout_model&) = default;
+      binary_readout_model(binary_readout_model&&) = default;
+      binary_readout_model &operator=(const binary_readout_model&) = default;
+      binary_readout_model &operator=(binary_readout_model&&) = default;
+      bool predict(const tensor &x) {
+        return fc.predict(x).as_scalar() > 0;
+      }
+
+      tensor compute_loss(const tensor &x, bool oracle) {
+        return dy::binary_log_loss(dy::logistic(fc.predict(x)), oracle);
+      }
+    };
+  }
+}
+
+#endif //DYNET_WRAPPER_DY_BINARY_READOUT_MODEL_HPP
