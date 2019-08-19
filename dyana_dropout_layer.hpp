f1a50717a2e30c6c274af7e61817ec76e3561a79
YAN Yuchen <cirahnitex@gmail.com>
2019-08-19 16:35:06 +0800

    fix default dropout rate

--- dyana_dropout_layer.hpp
+++ dyana_dropout_layer.hpp
@@ -12,6 +12,7 @@
 namespace dyana {
   /**
    * a layer that performs dropout operation
+   * when the dropout happens, the other undropped values will be scaled up by 1/p
    * this layer has no effect if not training
    */
   class dropout_layer {
@@ -22,7 +23,11 @@ namespace dyana {
     dropout_layer(dropout_layer&&) noexcept = default;
     dropout_layer &operator=(const dropout_layer&) = default;
     dropout_layer &operator=(dropout_layer&&) noexcept = default;
-    dropout_layer(float dropout_rate = 0.9):dropout_rate(dropout_rate) {};
+    /**
+     * create a dropout layer
+     * \param dropout_rate the possibility that a value may be set to zero
+     */
+    dropout_layer(float dropout_rate = 0.1):dropout_rate(dropout_rate) {};
     dyana::tensor operator()(const dyana::tensor& x) {
       return dyana::is_training()?dyana::dropout(x, dropout_rate):x;
     }
